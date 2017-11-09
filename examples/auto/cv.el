cb870f4ffbb59ebe85363ec30fb3e1c65b1e658b
chenchuanyin <chenchuanyinuestc@gmail.com>
2017-11-09 08:57:07 +0800

    resume

---
+++ examples/auto/cv.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "cv"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("awesome-cv" "11pt" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "cv/education"
+    "cv/skills"
+    "cv/experience"
+    "cv/honors"
+    "awesome-cv"
+    "awesome-cv11"))
+ :latex)
+
